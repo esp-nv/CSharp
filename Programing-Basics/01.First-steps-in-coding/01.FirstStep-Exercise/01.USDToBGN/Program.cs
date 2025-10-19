772222cc30ce52534433458b262c8727e33c1a82
esp-nv <esp_nv@abv.bg>
2025-10-19 09:45:20 +0300

    del files

--- Programing-Basics/01.First-steps-in-coding/01.FirstStep-Exercise/01.USDToBGN/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿//Напишете програма за конвертиране на щатски долари (USD) в български лева (BGN).
-//Използвайте фиксиран курс между долар и лев: 1 USD = 1.79549 BGN.
-
-double usd = double.Parse(Console.ReadLine());
-
-double convert = usd * 1.79549;
-
-Console.WriteLine(convert);

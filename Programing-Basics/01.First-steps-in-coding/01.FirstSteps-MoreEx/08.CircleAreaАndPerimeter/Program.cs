662d6beb539d6b4c036e4ce2bd0e199d2f1e3b88
esp-nv <esp_nv@abv.bg>
2025-10-19 09:31:38 +0300

    01.FirstSteps-MoreEx

---
+++ Programing-Basics/01.First-steps-in-coding/01.FirstSteps-MoreEx/08.CircleAreaАndPerimeter/Program.cs
@@ -0,0 +1,10 @@
+﻿//08.Circle Area and Perimeter
+
+double r = double.Parse(Console.ReadLine());
+
+
+double area = Math.PI * r * r;
+double perimeter = 2 * r * Math.PI;
+
+Console.WriteLine($"{area:f2}");
+Console.WriteLine($"{perimeter:f2}");

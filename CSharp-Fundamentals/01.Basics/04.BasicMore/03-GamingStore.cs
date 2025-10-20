f1117b0a0472cac1e46a9630f1be95b499c6db45
esp-nv <esp_nv@abv.bg>
2025-10-21 00:25:10 +0300

    04.BasicMore

---
+++ CSharp-Fundamentals/01.Basics/04.BasicMore/03-GamingStore.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+namespace _03_GamingStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double startBalance = balance;
+            double spentMoney = 0;
+            string input = "";
+            bool isFound = false;
+            bool isGameTime = false;
+
+            while ((input = Console.ReadLine()) != "Game Time")
+            {
+                double price = 0;
+
+                switch (input)
+                {
+                    case "OutFall 4":
+                        price = 39.99;
+                        isFound = true;
+                        break;
+                    case "CS: OG":
+                        price = 15.99;
+                        isFound = true;
+                        break;
+                    case "Zplinter Zell":
+                        isFound = true;
+                        price = 19.99;
+                        break;
+                    case "Honored 2":
+                        isFound = true;
+                        price = 59.99;
+                        break;
+                    case "RoverWatch":
+                        isFound = true;
+                        price = 29.99;
+                        break;
+                    case "RoverWatch Origins Edition":
+                        isFound = true;
+                        price = 39.99; break;
+                    default:
+                        Console.WriteLine("Not Found");
+                        isFound = false;
+                        break;
+                }
+
+                if (balance >= price && isFound == true)
+                {
+                    balance -= price;
+                    spentMoney += price;
+                    Console.WriteLine($"Bought {input}");
+
+                }
+                else if (balance < price && isFound == true)
+                {
+                    Console.WriteLine("Too Expensive");
+                }
+
+                if (isGameTime == true && balance == 0)
+                {
+                    break;
+                }
+
+            }
+
+            if (balance != 0)
+            {
+                Console.WriteLine($"Total spent: ${spentMoney:f2}. Remaining: ${balance:f2}");
+            }
+
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+                isGameTime = true;
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/01.Basics/04.BasicMore/05-Messages.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string message = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string number = Console.ReadLine();
+
+                if (number == "0")
+                {
+                    message += " ";
+                }
+                else
+                {
+                    int firstDigit = int.Parse(number[0].ToString());
+
+                    int length = number.Length;
+                    int offset = (firstDigit - 2) * 3;
+
+                    if (firstDigit == 8 || firstDigit == 9)
+                    {
+                        offset += 1;
+                    }
+
+                    int index = (offset + length) + 96;
+                    char letter = (char)(index);
+                    message += letter;
+                }
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}

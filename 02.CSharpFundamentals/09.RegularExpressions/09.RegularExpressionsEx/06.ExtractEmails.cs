552a5c21c86b89ad8ba3a2b72335d56d7e61c5fe
esp-nv <esp_nv@abv.bg>
2026-02-16 23:01:31 +0200

    Create 06.ExtractEmails.cs

---
+++ 02.CSharpFundamentals/09.RegularExpressions/09.RegularExpressionsEx/06.ExtractEmails.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"[^\.\-_]\b(?![\._\-])[A-Za-z0-9]+[\.\-_]*[A-Za-z0-9]+@[^\.\-](?:[A-Za-z\.\-]+\.)+[A-Za-z]+";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                Console.WriteLine(matches[i].Value.Trim(',', ' ', '\n'));
+            }
+        }
+    }
+}

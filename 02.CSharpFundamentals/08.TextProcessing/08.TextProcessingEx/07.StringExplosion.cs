2b89a9b9887cd38ea1b4356707dc491c2cbb78db
esp-nv <esp_nv@abv.bg>
2026-02-16 22:34:57 +0200

    Create 07.StringExplosion.cs

---
+++ 02.CSharpFundamentals/08.TextProcessing/08.TextProcessingEx/07.StringExplosion.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            string input = Console.ReadLine();
+
+            int explosionPower = 0;
+            string result = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar == '>')
+                {
+                    explosionPower += int.Parse(input[i + 1].ToString());
+                    result += currentChar;
+                }
+                else if (explosionPower > 0)
+                {
+                    explosionPower--;
+                    continue; // Skip adding the character to the result
+                }
+                else
+                {
+                    result += currentChar;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

5fd0231a6faef7ee22f4d5ab13b2457a541b9166
esp-nv <esp_nv@abv.bg>
2026-02-16 22:45:57 +0200

    Update 00.LongestPalindromeSubList.cs

--- 02.CSharpFundamentals/08.TextProcessing/08.TextProcessingMore/00.LongestPalindromeSubList.cs
+++ 02.CSharpFundamentals/08.TextProcessing/08.TextProcessingMore/00.LongestPalindromeSubList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace _00.LongestPalindromeSub_List
+namespace _00.LongestPalindromeSubList
 {
     internal class Program
     {

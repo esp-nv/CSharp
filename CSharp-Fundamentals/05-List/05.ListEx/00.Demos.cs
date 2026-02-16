ef813b3857cdd18834a3dc17ce90fb82c3dc7123
esp-nv <esp_nv@abv.bg>
2026-02-16 18:52:10 +0200

    Refactor input reversal and streamline processing

---
+++ CSharp-Fundamentals/05-List/05.ListEx/00.Demos.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _00.Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split('|').ToList();
+            input.Reverse(); 
+            List<int> result = new List<int>();
+
+            foreach (var item in input)
+            {               
+                result.AddRange(item.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}

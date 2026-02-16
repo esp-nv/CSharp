ac8d73b63c682804211762c035f30e0baadcf669
esp-nv <esp_nv@abv.bg>
2026-02-16 14:42:31 +0200

    Add AddAndSubtract program to calculate result

---
+++ CSharp-Fundamentals/04.Methods/04.Methods-Exercise/05.AddAndSubtract.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(PrintResult(firstNumber, secondNumber, thirdNumber));
+        }
+        
+        private static int PrintResult(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return SumResult(firstNumber, secondNumber) - thirdNumber;
+        }
+
+        private static int SumResult(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}

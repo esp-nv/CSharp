4177348effe76f77b658b15b544978326cf4cce7
esp-nv <esp_nv@abv.bg>
2026-02-16 22:10:24 +0200

    Create 06.StudentAcademy.cs

---
+++ 02.CSharpFundamentals/07.AssociativeArrays/07.AssociativeArraysEx/06.StudentAcademy.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());  
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students[studentName] = new List<double>();
+                }
+                students[studentName].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                double averageGrade = student.Value.Average();
+
+                if (averageGrade >= 4.5)
+                {
+                    Console.WriteLine($"{student.Key} -> {averageGrade:F2}");
+                }
+            }
+        }
+    }
+}

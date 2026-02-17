b7f849f96d01dc34ec2da1e846fa744cb93c086b
esp-nv <esp_nv@abv.bg>
2026-02-17 20:20:21 +0200

    Create Lab03.Largest3Numbers.cs

---
+++ 03.CSharpAdvanced/03.SetsAndDictionariesAdvanced/Lab03.Largest3Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sorted = input.OrderByDescending(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", sorted.Take(3)));
+        }
+    }
+}

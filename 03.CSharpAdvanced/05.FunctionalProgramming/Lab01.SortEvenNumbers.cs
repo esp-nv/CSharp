190ba3f1403fe19963a1234f24b9282eda7e0fd4
esp-nv <esp_nv@abv.bg>
2026-02-17 21:43:33 +0200

    Create Lab01.SortEvenNumbers.cs

---
+++ 03.CSharpAdvanced/05.FunctionalProgramming/Lab01.SortEvenNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x %2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+            Console.WriteLine(string.Join(", ",number));
+        }
+    }
+}

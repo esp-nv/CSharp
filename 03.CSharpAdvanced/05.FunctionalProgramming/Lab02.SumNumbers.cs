d3c1b932de4abf7a68c95a99fff78b4e82fb8677
esp-nv <esp_nv@abv.bg>
2026-02-17 21:44:40 +0200

    Create Lab02.SumNumbers.cs

---
+++ 03.CSharpAdvanced/05.FunctionalProgramming/Lab02.SumNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

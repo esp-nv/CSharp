30c22772f545f3ee0cb9be02e97f45dacbe4ccb2
esp-nv <esp_nv@abv.bg>
2026-02-17 19:39:14 +0200

    Create 02.SquaresInMatrix.cs

---
+++ 03.CSharpAdvanced/02.MultidimensionalArray/Exercice/02.SquaresInMatrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int rows = nums[0];
+            int cols = nums[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] symbol = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = symbol[col];
+                }
+            }
+
+            int foundedMatrix = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        foundedMatrix++;
+                    }
+                }
+            }
+
+            Console.WriteLine(foundedMatrix);
+        }
+    }
+}

c5b34ef026fa190750ff12129855f4923af00645
esp-nv <esp_nv@abv.bg>
2026-02-17 20:23:20 +0200

    Create Lab04.ProductShop.cs

---
+++ 03.CSharpAdvanced/03.SetsAndDictionariesAdvanced/Lab04.ProductShop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab04.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            SortedDictionary<string,Dictionary<string,double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+            string input;
+
+            while ((input=Console.ReadLine())!= "Revision")
+            {
+                               
+                string[] currInput = input.Split(", ");
+                string shop = currInput[0];
+                string food = currInput[1];
+                double price = double.Parse(currInput[2]);
+
+                if (!shops.ContainsKey(shop)) 
+                {
+                    shops.Add(shop,new Dictionary<string,double>());
+                }
+
+                if (!shops[shop].ContainsKey(food))
+                {
+                    shops[shop].Add(food,price);
+                }                
+            }
+
+            foreach (var kvp in shops)
+            { 
+                var shop = kvp.Key;
+                var food = kvp.Value;
+
+                Console.WriteLine($"{shop}->");
+
+                foreach (var item in food)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+        }
+    }
+}

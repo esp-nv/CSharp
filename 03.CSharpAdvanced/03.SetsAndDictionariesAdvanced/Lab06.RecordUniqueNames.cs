0bbe4f1445f99e8cedd06ef370fafb118608b4f5
esp-nv <esp_nv@abv.bg>
2026-02-17 20:26:44 +0200

    Create Lab06.RecordUniqueNames.cs

---
+++ 03.CSharpAdvanced/03.SetsAndDictionariesAdvanced/Lab06.RecordUniqueNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab06.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var names = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

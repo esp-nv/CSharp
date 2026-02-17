2c9da458545032e7d2d6e0ac3b6791e22f596423
esp-nv <esp_nv@abv.bg>
2026-02-17 10:22:33 +0200

    Create 06.ConcatenateData.cs

---
+++ 01.CSharpProgramingBasics/01.FirstStepsInCoding/02.FirstStepsLab/06.ConcatenateData.cs
@@ -0,0 +1,8 @@
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+string town = Console.ReadLine();
+
+//Console.WriteLine("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".");
+// towa e 2ri variant na rezultat
+Console.WriteLine($"You are {firstName} { lastName}, a {age}-years old person from {town}.");

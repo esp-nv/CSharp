e5f196bc9deb7e90835742b46d033a9c3c96bd0b
esp-nv <esp_nv@abv.bg>
2026-02-17 19:58:45 +0200

    Create Lab01.CountSameValuesInArray.cs

---
+++ 03.CSharpAdvanced/03.SetsAndDictionariesAdvanced/Lab01.CountSameValuesInArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double,int> dic = new Dictionary<double,int>();
+
+            foreach (var num in nums)
+            {                
+                if (!dic.ContainsKey(num))
+                {
+                    dic[num] = 0;
+                }
+
+                dic[num]++;
+            }
+
+            foreach(var num in dic)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}

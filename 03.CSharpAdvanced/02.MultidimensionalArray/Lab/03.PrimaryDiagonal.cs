a017f25de0a7c2465fd8e7192e38566f18e3b85c
esp-nv <esp_nv@abv.bg>
2026-02-17 19:02:46 +0200

    Create 03.PrimaryDiagonal.cs

---
+++ 03.CSharpAdvanced/02.MultidimensionalArray/Lab/03.PrimaryDiagonal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] currRow = ParceArrayFromConsole(' ');
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row,col] = currRow[col];
+                }              
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < n; row++)
+            {                
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        sum+= matrix[row, col];                 
+                    }
+                }               
+            }
+            
+            Console.WriteLine(sum);
+        }
+        static int[] ParceArrayFromConsole(params char[] splitSymbols)
+        {
+            return Console.ReadLine()
+                  .Split(splitSymbols, StringSplitOptions.RemoveEmptyEntries)
+                  .Select(int.Parse)
+                  .ToArray();
+
+        }
+    }
+}

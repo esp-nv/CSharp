278ae800ddabb21bde0ba95d22cace633d4c2648
esp-nv <esp_nv@abv.bg>
2026-02-17 21:45:23 +0200

    Create Lab03.CountUppercaseWords.cs

---
+++ 03.CSharpAdvanced/05.FunctionalProgramming/Lab03.CountUppercaseWords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => char.IsUpper(word[0]))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}

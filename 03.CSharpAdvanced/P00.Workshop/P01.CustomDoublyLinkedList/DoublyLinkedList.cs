8451c9d8c5b0c7eea008d0dd415e752ff8884e5f
esp-nv <esp_nv@abv.bg>
2026-04-10 12:15:07 +0300

    P01.CustomDoublyLinkedList

---
+++ 03.CSharpAdvanced/P00.Workshop/P01.CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+    }
+}
---
+++ 03.CSharpAdvanced/P00.Workshop/P01.CustomDoublyLinkedList/ListNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.CustomDoublyLinkedList
+{
+    public class ListNode
+    {
+        public ListNode(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; set; }
+
+        public ListNode NextNode { get; set; }
+
+
+        public ListNode PreviousNode { get; set; }
+
+    }
+}

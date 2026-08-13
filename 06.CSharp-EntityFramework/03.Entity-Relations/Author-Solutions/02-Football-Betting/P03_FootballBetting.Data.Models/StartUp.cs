6a68f9e5e0375ab5631424072c3f5b74ac31ddd5
esp-nv <esp_nv@abv.bg>
2026-08-13 22:40:48 +0300

    entity relations

---
+++ 06.CSharp-EntityFramework/03.Entity-Relations/Author-Solutions/02-Football-Betting/P03_FootballBetting.Data.Models/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}

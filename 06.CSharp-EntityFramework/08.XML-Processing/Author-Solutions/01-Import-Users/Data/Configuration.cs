f7ad36972ba31d710a0a6d9e46507a1600b5d35e
esp-nv <esp_nv@abv.bg>
2026-08-13 22:34:06 +0300

    import users - xml

---
+++ 06.CSharp-EntityFramework/08.XML-Processing/Author-Solutions/01-Import-Users/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=.;Database=ProductShop;Integrated Security=True;Encrypt=False";
+
+        public const string xmlFolderInsert = @"C:\My files\softuni\C# Entity Framework\Extensible Markup Language - XML 2\ProductShop\Datasets";
+    }
+}
---
+++ 06.CSharp-EntityFramework/08.XML-Processing/Author-Solutions/01-Import-Users/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+            //Console.WriteLine("DB is created!!!!!");
+
+            string xml = File.ReadAllText(Configuration.xmlFolderInsert + @"\users.xml");
+            string result = ImportUsers(context, xml);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            ImportUsersDTO[] xmlUsers = Deserialize<ImportUsersDTO[]>(inputXml, "Users");
+
+            User[] users = xmlUsers.Select(u => new User
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Age = u.Age
+            }).ToArray();
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+
+
+
+        private static T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T dtos = (T)xmlSerializer
+                .Deserialize(reader);
+
+            return dtos;
+        }
+    }
+}

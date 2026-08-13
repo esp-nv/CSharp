b65e03a130a5b48470c7efd2e534395824dce591
esp-nv <esp_nv@abv.bg>
2026-08-13 22:36:32 +0300

    json

---
+++ 06.CSharp-EntityFramework/07.JSON-Processing/Author-Solutions/08-Export-Users-and-Products/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        public Category()
+        {
+            this.CategoriesProducts = new List<CategoryProduct>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(15)]
+        public string Name { get; set; }
+
+        public ICollection<CategoryProduct> CategoriesProducts { get; set; }
+    }
+}
